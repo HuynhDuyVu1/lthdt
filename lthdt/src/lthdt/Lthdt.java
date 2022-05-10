47578a07cbeedb0abd74b1f734a293b76affaeb9
HuynhDuyVu1 <vuhuynh459748@gmail.com>
2022-05-10 22:53:35 +0700

    first commit

--- lthdt/src/lthdt/Lthdt.java
+++ lthdt/src/lthdt/Lthdt.java
@@ -17,7 +17,6 @@ public class Lthdt {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!!!");
-       
     }
     
 }
