0f2f14158c22ec91a925b17ec405ea65574570fb
HuynhDuyVu1 <vuhuynh459748@gmail.com>
2022-05-10 23:00:27 +0700

    first commit

---
+++ lthdt/src/lthdt/donglenh/chuong1/Worked.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lthdt.donglenh.chuong1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author vuhuy
+ */
+public class Worked extends Person {
+    private String congty;
+
+    public Worked() {
+    }
+
+    public Worked(String congty, String hoten, int gioitinh, Date ngaysinh) {
+        super(hoten, gioitinh, ngaysinh);
+        this.congty = congty;
+    }
+
+    public String getCongty() {
+        return congty;
+    }
+
+    public void setCongty(String congty) {
+        this.congty = congty;
+    }
+    
+    
+    
+    
+}
